778c0d1b7309fe315c73eb1806a049b189f671ea
Yuri Gabriel <yurigabriel1995@outlook.com>
2023-07-19 17:02:32 -0300

    Project created

---
+++ src/test/java/com/yuri/barber/barber/BarberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.barber.barber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
